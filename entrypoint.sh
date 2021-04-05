ada6fb553ecfb46206e1e9410cef63ff4a2df9f2
Elliot Forbes <emforce@live.com>
2021-04-05 11:31:32 +0100

    Testing notarization

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,17 @@
 
 set -euox pipefail
 
-echo $VCN_USER
-echo $VCN_PASSWORD
+setup() {
+    curl -L -o /tmp/vcn https://github.com/vchain-us/vcn/releases/download/v0.9.4/vcn-v0.9.4-linux-amd64-static
+    CHECKSUM=$(sha256sum /tmp/vcn | cut -d " " -f 1)
+    curl -s https://api.codenotary.io/authenticate/$CHECKSUM?org=vchain.us | grep -q :0
+    chmod +x /tmp/vcn
+}
 
-echo "hello world"
+main() {
+    echo | /tmp/vcn login
+    /tmp/vcn n -p $1
+}
+
+setup
+main $1

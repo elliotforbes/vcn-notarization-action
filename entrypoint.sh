6d82b6039fcf603e02aeea393f1c0901f6468f6b
Elliot Forbes <emforce@live.com>
2021-04-05 18:51:00 +0100

    Mon Apr  5 18:51:00 BST 2021

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,16 +3,14 @@
 set -euox pipefail
 
 setup() {
-    curl -L -o /tmp/vcn https://github.com/vchain-us/vcn/releases/download/v0.9.4/vcn-v0.9.4-linux-amd64-static
+    curl -L -o /tmp/vcn https://github.com/codenotary/vcn/releases/download/v0.9.4/vcn-v0.9.4-linux-amd64-static
     CHECKSUM=$(sha256sum /tmp/vcn | cut -d " " -f 1)
-    curl -s https://api.codenotary.io/authenticate/$CHECKSUM?org=vchain.us | grep -q :0
+    curl -s https://api.codenotary.io/authenticate/$CHECKSUM?org=codenotary.com | grep -q :0
     chmod +x /tmp/vcn
 }
 
 main() {
     echo | /tmp/vcn login
-    /tmp/vcn n -p $1
 }
 
-setup
-main $1
+echo "hello world"

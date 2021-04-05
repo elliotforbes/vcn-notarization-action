0f19bfd6c460695dd595f32fb6ce7a1d3888aa9b
Elliot Forbes <emforce@live.com>
2021-04-05 11:13:04 +0100

    Initial commit

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,23 +1,5 @@
 #!/bin/bash
 
-set -eoux pipefail
+set -euox pipefail
 
-setup() {
-    curl -L -o /tmp/vcn https://github.com/vchain-us/vcn/releases/download/v0.9.4/vcn-v0.9.4-linux-amd64-static
-    CHECKSUM=$(sha256sum /tmp/vcn | cut -d " " -f 1)
-    curl -s https://api.codenotary.io/authenticate/$CHECKSUM?org=vchain.us | grep -q :0
-    chmod +x /tmp/vcn
-}
-
-main() { 
-    echo | /tmp/vcn login
-    /tmp/vcn n -p $1
-}
-
-if [ $# -eq 0 ];
-then
-    exit 0
-fi
-
-setup
-main $1
+echo "hello world"
